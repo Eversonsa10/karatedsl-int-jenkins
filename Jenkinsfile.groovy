f9c20e2df97b5e14c220bb9eed577f610246d02c
Souza de Araujo <e.souza.de.araujo@accenture.com>
2024-09-23 22:43:28 -0300

    Ajuste no jenkinsfile 02

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,19 +2,6 @@ pipeline {
     agent any
 
     stages {
-
-        stage('Test Stage') {
-            steps {
-                script {
-                    def mvnHome = tool name: 'maven_3_9_9', type: 'maven'
-                    withEnv(["PATH+MAVEN=${mvnHome}/bin"]) {
-                        bat "${mvnHome}\\bin\\mvn test -Dtest=PostsRunner"
-                    }
-                }
-            }
-
-        }
-
         stage('Cucumber Reports') {
             steps {
                 cucumber buildStatus: "UNSTABLE",
