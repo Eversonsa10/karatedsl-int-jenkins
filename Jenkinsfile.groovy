da9314e065fc86d7c2a7ebdf6b341c1c43b3ffdf
Souza de Araujo <e.souza.de.araujo@accenture.com>
2024-09-23 22:41:13 -0300

    Ajuste no Runnersk

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,18 +3,6 @@ pipeline {
 
     stages {
 
-        stage('Compile Stage') {
-            steps {
-                script {
-                    def mvnHome = tool name: 'maven_3_9_9', type: 'maven'
-                    withEnv(["PATH+MAVEN=${mvnHome}/bin"]) {
-                        bat "${mvnHome}\\bin\\mvn clean compile"
-                    }
-                }
-
-            }
-        }
-
         stage('Test Stage') {
             steps {
                 script {
